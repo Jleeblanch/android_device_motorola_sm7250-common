LineageOS/android_device_motorola_sdm660-common

sdm660-common: Add support for device specific libinit
Change-Id: I39400afba24b6467ac62bc9cf661cc2050517d69

soong/init.go
@@ -1,0 +1,55 @@
+package sdm660
+
+import (
+    "android/soong/android"
+    "android/soong/cc"
+    "strings"
+)
+
+func initFlags(ctx android.BaseContext) []string {
+    var cflags []string
+
+    var config = ctx.AConfig().VendorConfig("MOTOROLA_SDM660_INIT")
+    var lib = strings.TrimSpace(config.String("DEVICE_LIB"))
+
+    if len(strings.TrimSpace(lib)) != 0 {
+        cflags = append(cflags, "-DTARGET_PROVIDES_DEVICE_LIBINIT")
+    }
+    return cflags
+}
+
+func initStaticLibs(ctx android.BaseContext) []string {
+    var staticlibs []string
+
+    var config = ctx.AConfig().VendorConfig("MOTOROLA_SDM660_INIT")
+    var lib = strings.TrimSpace(config.String("DEVICE_LIB"))
+
+    if len(strings.TrimSpace(lib)) != 0 {
+        staticlibs = append(staticlibs, lib)
+    }
+    return staticlibs
+}
+
+func initLibrary(ctx android.LoadHookContext) {
+    type props struct {
+        Target struct {
+            Android struct {
+                Cflags []string
+                Whole_static_libs []string
+            }
+        }
+    }
+
+    p := &props{}
+    p.Target.Android.Cflags = initFlags(ctx)
+    p.Target.Android.Whole_static_libs = initStaticLibs(ctx)
+    ctx.AppendProperties(p)
+}
+
+func initLibraryFactory() android.Module {
+    module, library := cc.NewLibrary(android.HostAndDeviceSupported)
+    library.BuildOnlyStatic()
+    newMod := module.Init()
+    android.AddLoadHook(newMod, initLibrary)
+    return newMod
+}

soong/main.go
@@ -1,0 +1,9 @@
+package sdm660
+
+import (
+    "android/soong/android"
+)
+
+func init() {
+    android.RegisterModuleType("motorola_sdm660_init_library_static", initLibraryFactory)
+}

Jleeblanch/android_device_motorola_sm7250-common

soong/init.go
@@ -1,0 +1,55 @@
+package sm7250
+
+import (
+    "android/soong/android"
+    "android/soong/cc"
+    "strings"
+)
+
+func initFlags(ctx android.BaseContext) []string {
+    var cflags []string
+
+    var config = ctx.AConfig().VendorConfig("MOTOROLA_SM7250_INIT")
+    var lib = strings.TrimSpace(config.String("DEVICE_LIB"))
+
+    if len(strings.TrimSpace(lib)) != 0 {
+        cflags = append(cflags, "-DTARGET_PROVIDES_DEVICE_LIBINIT")
+    }
+    return cflags
+}
+
+func initStaticLibs(ctx android.BaseContext) []string {
+    var staticlibs []string
+
+    var config = ctx.AConfig().VendorConfig("MOTOROLA_SM7250_INIT")
+    var lib = strings.TrimSpace(config.String("DEVICE_LIB"))
+
+    if len(strings.TrimSpace(lib)) != 0 {
+        staticlibs = append(staticlibs, lib)
+    }
+    return staticlibs
+}
+
+func initLibrary(ctx android.LoadHookContext) {
+    type props struct {
+        Target struct {
+            Android struct {
+                Cflags []string
+                Whole_static_libs []string
+            }
+        }
+    }
+
+    p := &props{}
+    p.Target.Android.Cflags = initFlags(ctx)
+    p.Target.Android.Whole_static_libs = initStaticLibs(ctx)
+    ctx.AppendProperties(p)
+}
+
+func initLibraryFactory() android.Module {
+    module, library := cc.NewLibrary(android.HostAndDeviceSupported)
+    library.BuildOnlyStatic()
+    newMod := module.Init()
+    android.AddLoadHook(newMod, initLibrary)
+    return newMod
+}

soong/main.go
@@ -1,0 +1,9 @@
+package sm7250
+
+import (
+    "android/soong/android"
+)
+
+func init() {
+    android.RegisterModuleType("motorola_sm7250_init_library_static", initLibraryFactory)
+}